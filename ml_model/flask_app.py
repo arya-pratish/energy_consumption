205eaf741c6480ba68839a7c2f7d2ab8aa9f1dac
arya-pratish <aryapratish22@gmail.com>
2025-05-28 16:10:08 +0000

    add 'health' in flask_app

--- ml_model/flask_app.py
+++ ml_model/flask_app.py
@@ -36,6 +36,9 @@ def predict():
 
     return render_template("result.html", prediction=formatted_prediction)
 
+@app.route('/health')
+def health():
+    return 'OK', 200
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
