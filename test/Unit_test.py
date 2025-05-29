edc743c9381c20bda42e7626979b97c5ed3c2862
arya-pratish <aryapratish22@gmail.com>
2025-05-29 21:59:13 +0000

    test@12

--- test/Unit_test.py
+++ test/Unit_test.py
@@ -6,11 +6,11 @@ sys.path.insert(1, '/home/runner/work/energy_consumption/energy_consumption')
 from ml_model.preprocessing import con_str_num
 
 
-def check_func_digit():
+def test_func_digit():
     assert con_str_num('900')== 9000, "Fail to convert into numeric"
 
-def check_func_null():
+def test_func_null():
     assert con_str_num('') is pd.NA, "Fail to identify NULL value"
 
-def check_func_str():
+def test_func_str():
     assert con_str_num('Jack')== 'Jack', "Fail to identify string value"
