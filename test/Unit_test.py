10050fd81700ec610c5a1435bc37ee85a9c08fea
arya-pratish <aryapratish22@gmail.com>
2025-05-29 21:17:37 +0000

    test@6

--- test/Unit_test.py
+++ test/Unit_test.py
@@ -1,8 +1,10 @@
 import sys
+import pandas as pd
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, '/home/runner/work/energy_consumption/energy_consumption')
 
 from ml_model.preprocessing import con_str_num
+
 
 def check_func_digit():
     assert con_str_num('9000')== 9000, "Fail to convert into numeric"
