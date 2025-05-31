318c4db53008cd5d18f58cbc4eb4d2d06b443cb6
arya-pratish <aryapratish22@gmail.com>
2025-05-31 11:08:06 +0000

    fix the bug in test_1

--- test/test_1.py
+++ test/test_1.py
@@ -7,5 +7,7 @@ from ml_model.preprocessing import pre_process
 
 def test_preprocess():
     cols_name = ['Building Type', 'Square Footage', 'Number of Occupants', 'Appliances Used', 'Average Temperature', 'Day of Week','Energy Consumption']
-    df = pd.DataFrame([['Residential', 4323  ,20, 56,  '22  '  , 'Weekday'  , 4212]], columns=cols_name )          
-    assert pre_process(df).all(0, 4323  ,20, 56,  22  ,  0 , 4212), "Fail to pre-processed"
+    df = pd.DataFrame([['Residential', 4323  ,20, 56,  '22  '  , 'Weekday'  , 4212]], columns=cols_name )   
+    df = pre_process(df)
+    df_1 = pd.DataFrame([[0, 4323  ,20, 56,  22  ,  0 , 4212]], columns=cols_name )        
+    assert df.equals(df_1), "Fail to pre-processed"
