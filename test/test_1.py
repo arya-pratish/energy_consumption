197d4f7a2e035aa7d9b68aea86795171b5945826
arya-pratish <aryapratish22@gmail.com>
2025-05-31 10:55:50 +0000

    update test_1.py

--- test/test_1.py
+++ test/test_1.py
@@ -6,6 +6,6 @@ from ml_model.preprocessing import pre_process
 
 
 def test_preprocess():
-    feature_names = ['Building Type', 'Square Footage', 'Number of Occupants', 'Appliances Used', 'Average Temperature', 'Day of Week','Energy Consumption']
-    df = pd.DataFrame(['Residential', 4323  ,20, 56,  '22  '  , 'Weekday'  , 4212], columns=feature_names )          
-    assert pre_process(df)== [0, 4323  ,20, 56,  22  ,  0 , 4212], "Fail to convert into numeric"
+    cols_name = ['Building Type', 'Square Footage', 'Number of Occupants', 'Appliances Used', 'Average Temperature', 'Day of Week','Energy Consumption']
+    df = pd.DataFrame([['Residential', 4323  ,20, 56,  '22  '  , 'Weekday'  , 4212]], columns=cols_name )          
+    assert pre_process(df)== [0, 4323  ,20, 56,  22  ,  0 , 4212], "Fail to pre-processed"
